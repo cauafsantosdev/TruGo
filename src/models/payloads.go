707255a4fb1b7caa71a0653b03492c030ec1d40a
Steven <stevenndev8@gmail.com>
2025-07-09 00:33:59 -0300

    feat: payloads para resposta do estado do jogo atual (cartas jogadas, apostas disponíveis,...)

--- src/models/payloads.go
+++ src/models/payloads.go
@@ -33,3 +33,21 @@ type EntrouSalaResposta struct {
 type SalasDisponiveis struct {
 	SalasDisponiveis map[string]int `json:"salasDisponiveis"`
 }
+
+type CartaResposta struct {
+	Valor int    `json:"valor"`
+	Naipe string `json:"naipe"`
+	Forca int    `json:"forca"`
+}
+
+type Jogada struct {
+	IDEquipe    string `json:"idEquipe"`
+	JogadorNome string `json:"jogador"`
+	CartaJogada CartaResposta `json:"cartaJogada"`
+}
+
+type StatusRodada struct {
+	Type              string          `json:"type"`
+	CartasJogadas     []Jogada        `json:"cartasJogadas"`
+	ApostasDiponiveis map[string]bool `json:"apostasDisponiveis"`
+}
